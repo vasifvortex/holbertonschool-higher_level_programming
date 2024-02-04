9ed8637e242f935597e0356cb2db2c55b5380e7a
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-02-04 16:58:01 +0400

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-"""text indent"""
-
-
 def text_indentation(text):
     """text indent"""
     if not isinstance(text, str):
@@ -14,14 +10,3 @@ def text_indentation(text):
 
     print("Actual stdout:", result)
     return result
-
-if __name__ == "__main__":
-    import doctest
-    text = "Holberton School"
-    print("Desired stdout:", text)
-    print("Desired stdout length:", len(text))
-    
-    result = text_indentation(text)
-    print("Actual stdout length:", len(result))
-
-    doctest.testfile("tests/5-text_indentation.txt")
