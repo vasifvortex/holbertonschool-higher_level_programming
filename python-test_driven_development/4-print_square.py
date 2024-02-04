308a629cd7c1388db72dbb4e56cda83a3087c9ff
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-02-04 14:15:05 +0400

    Update 4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""print square"""
+
+
 def print_square(size):
+    """print square"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
