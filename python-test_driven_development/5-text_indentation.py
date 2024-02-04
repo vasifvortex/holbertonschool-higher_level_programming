ed96fb293265820c430dd7bc88f2c365b8b3a8ce
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-02-04 15:17:26 +0400

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,6 +10,6 @@ def text_indentation(text):
         words = (delimeter + "\n\n").join(
                 [index.strip(" ") for index in words.split(delimeter)])
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
+###if __name__ == "__main__":
+###    import doctest
+###    doctest.testfile("tests/5-text_indentation.txt")
