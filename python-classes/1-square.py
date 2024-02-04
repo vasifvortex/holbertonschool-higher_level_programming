1d4f6ae10086e9d8f127e731a9fafd7aacb84079
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-02-04 13:34:13 +0400

    Create 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Documentation"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size) -> None:
+        """
+        Initializes a new Square instance.
+
+        Args:
+        size (int): The size of a side of the square.
+        """
+        self.__size = size
