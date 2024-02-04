83ba9631a1d7891be0169b5141e811a2f2a50cfb
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-02-04 14:59:33 +0400

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,7 +7,8 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for delimeter in " ":
-        words = delimeter
+        words = delimeter.join(
+                [index.strip(" ") for index in words.split(delimeter)])
     for delimeter in "?:.":
         words = (delimeter + "\n\n").join(
                 [index.strip(" ") for index in words.split(delimeter)])
