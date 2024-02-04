baabf14dd9abac2b71fd69d8ad1a6ed561a4812f
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-02-04 17:15:27 +0400

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
-"""text indent"""
+"""This module defines the text_indentation function
+"""
 
 
 def text_indentation(text):
-    """Indent text based on delimiters"""
-    if not isinstance(text, str):
+    """ Function that prints indented text.
+    Args:
+        text (str): text to print
+    """
+    if type(text) != str:
         raise TypeError("text must be a string")
-
-    result = text
-    for delimiter in "?:.":
-        lines = result.split(delimiter)
-        result = (delimiter + "\n\n").join([line.strip() for line in lines])
-
-    print(result)
-    return(result)
+    flag = 0
+    newstring = ""
+    for i in text:
+        if flag == 1:
+            newstring = ""
+            flag = 0
+        if i not in "?:.":
+            newstring += i
+        else:
+            newstring += i
+            print(newstring.strip())
+            print()
+            flag = 1
+    if flag == 0 and '\n' not in newstring:
+        print(newstring.strip(), end="")
+    elif flag == 0:
+        print(newstring.strip())
