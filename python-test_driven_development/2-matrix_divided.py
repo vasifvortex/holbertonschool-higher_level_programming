8c11134709f10513f8dde9b7aea4a8b50f7c6d7f
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-02-04 14:42:21 +0400

    Update 2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,7 @@
 def matrix_divided(matrix, div):
     """matrix divided"""
     errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
-    if matrix == noneL
+    if matrix == "noneL":
         raise TypeError(errorMessage + " more characters")
     if not matrix:
         raise TypeError(errorMessage)
