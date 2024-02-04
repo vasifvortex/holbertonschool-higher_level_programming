67b8b5e9ff76604f48f453cba38bebed3930f780
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-02-04 15:19:15 +0400

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,9 +7,8 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for delimeter in "?:.":
-        words = (delimeter + "\n\n").join(
-                [index.strip(" ") for index in words.split(delimeter)])
+        words = (delimeter + "\n\n").join([index.strip(" ") for index in words.split(delimeter)])
 
-###if __name__ == "__main__":
-###    import doctest
-###    doctest.testfile("tests/5-text_indentation.txt")
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
