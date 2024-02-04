04471c286b52ca8f05b1e867434ee925a9f899f1
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-02-04 13:35:30 +0400

    Create 5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3                                                                                    """documentation"""
+"""documentation"""
+
+
+class Square:
+    """Square class with private attribute size"""
+    def __init__(self, size=0) -> None:
+        """Initialize Square with size attribute"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Public instance method return Size
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Public instance method return Size
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
