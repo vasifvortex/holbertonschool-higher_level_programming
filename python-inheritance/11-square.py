64b4a3e7da6a5af3f009154c9739f04a588e193a
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-02-13 23:46:05 +0400

    Create 11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Doc"""
+
+
+baseRec = __import__("9-rectangle").Rectangle
+
+
+class Square(baseRec):
+    """Square Class"""
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
