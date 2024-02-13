c60dfc122bb8dc050335ec675ecfd92c1c2e4d6b
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-02-13 23:51:40 +0400

    Create 7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Doc"""
+
+
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    lst = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    lst = []
+
+lst.extend(sys.argv[1:])
+save_to_json_file(lst, "add_item.json")
