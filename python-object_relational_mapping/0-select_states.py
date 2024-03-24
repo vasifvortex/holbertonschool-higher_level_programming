3ccf9daea110e7dd728db4796fe83cee333a4b32
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-03-24 20:39:06 +0400

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-"""Lists states"""
+"""Documentation"""
+
 
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=username,
+                         passwd=password, db=database)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    rows = cursor.fetchall()
+    for row in rows:
         print(row)
-    cur.close()
+    cursor.close()
+    db.close()
