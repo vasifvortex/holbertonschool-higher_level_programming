94882484fab8d5fa7c65503d0b72b3eb97a23e91
Vasif <67205301+vasifvortex@users.noreply.github.com>
2024-03-24 21:05:42 +0400

    Update 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
-"""List all states"""
-from sys import argv
+"""
+Start link class to table in database
+"""
+
+
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sys import argv
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
+
+def main():
+    if len(argv) != 4:
+        return
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(username, password, database),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
+
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
+
     session.close()
+
+
+if __name__ == "__main__":
+    main()
